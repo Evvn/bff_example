435d7821f27ad549ed2ba5ae8baa64b3847e3399
davjones0 <davjones0@yahoo.com>
2019-05-16 20:38:29 -0400

    logging for transactions

--- src/extractor/intentMapping/doshiiMapping/webhook.js
+++ src/extractor/intentMapping/doshiiMapping/webhook.js
@@ -56,7 +56,11 @@ const catchWebhook = (payload, onSuccess) => {
           // cancel doshii order
           orderCommands['CANCEL_ORDER']({orderId: DOSHII_ID, doshiiLocationId: DOSHII_LOCATION_ID, status: 'cancelled'}, ()=>{});
           let og_transaction_id;
-          orderCommands['RETRIEVE_ORDER']({doshiiLocationId: DOSHII_LOCATION_ID, orderId: DOSHII_ID}, (res) => { og_transaction_id = res.transactions[0].id});
+          orderCommands['RETRIEVE_ORDER']({doshiiLocationId: DOSHII_LOCATION_ID, orderId: DOSHII_ID}, (res) => { 
+            og_transaction_id = res.transactions[0].id
+            console.log(res.transactions[0]);
+          });
+          console.log('trxId', og_transaction_id);
           // issue refund - ask AVC, eftpos refund option?
           // send refund order (negative balance for reconciliation)
           transactionCommands['CREATE_TRANSACTION']({orderId: DOSHII_ID, doshiiLocationId: DOSHII_LOCATION_ID, method: 'cash', prepaid: true, linkedTrxId: og_transaction_id, reference: og_transaction_id, amount: parseInt(order.ORDER_TOTAL) * -1}, ()=>{ console.log('refund placed')});
