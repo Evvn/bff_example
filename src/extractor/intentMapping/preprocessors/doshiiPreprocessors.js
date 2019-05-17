ff4bbaa5d9b287500379fb22a21a300f4ad9a642
davjones0 <davjones0@yahoo.com>
2019-05-16 22:41:48 -0400

    juice schooner

--- src/extractor/intentMapping/preprocessors/doshiiPreprocessors.js
+++ src/extractor/intentMapping/preprocessors/doshiiPreprocessors.js
@@ -35,23 +35,23 @@ export const createOrderPreprocess = (rawOrders, doshiiLocationId) => {
         items: Object.keys(items).map(itemKey => {
             const { ORDER_ITEM } = createOrder;
             const item = items[itemKey];
-            const options = !item.addOns ? [] : item.addOns
-                .map(addon => {
-                    return {
-                        name: 'addons', //addon.name,
-                        posId: '000010',//addon.DOSHII_POS_ID,
+            const options = !item.addOns ? [] : //item.addOns
+                //.map(addon => {
+                    [{
+                        name: 'Postmix Modifiers', //addon.name,
+                        posId: '193',//addon.DOSHII_POS_ID,
                         variants: [{
-                            name: 'vegan cheese',
-                            posId: '000143',
-                            price: 80, 
+                            name: 'Add Cordial',
+                            posId: '8502-15400',
+                            price: 0, 
                         }]//addon.PRICE,
-                    };
-                });
-            ORDER_ITEM.name = item.name;
+                    }];
+               // });
+            ORDER_ITEM.name = 'Juice Schooner'//item.name;
             ORDER_ITEM.unitPrice = item.price;
             ORDER_ITEM.totalBeforeSurcounts = item.price;
             ORDER_ITEM.totalAfterSurcounts = item.price;
-            ORDER_ITEM.posId = '8502-17308' //item.DOSHII_POS_ID;
+            ORDER_ITEM.posId = '8502-16300' //item.DOSHII_POS_ID;
             ORDER_ITEM.options = options;
             return ORDER_ITEM;
         })
