c05ed729d5f998833048d3b901d4089329df468e
Evan <evvn@outlook.com>
2019-06-04 14:07:56 +1000

    fix definition formatting

--- src/menu/menuTransformer.js
+++ src/menu/menuTransformer.js
@@ -1,65 +1,47 @@
 // eslint-disable-next-line import/prefer-default-export
-import wordsToSearch from '../../data/wordsToSearch.json';
-import definitions from '../../data/definitions.json';
+import wordsToSearch from "../../data/wordsToSearch.json";
 
-const transformMenuItem = (menuItem) => {
-
+const transformMenuItem = menuItem => {
   let description = menuItem.DESCRIPTION.en;
-  description = !description ? '' : description;
-  const justWords = description.split(' ');
-  const definedWords = [];
-  const wordDictionary = {};
+  description = !description ? "" : description;
+  const justWords = description.split(" ");
 
-  justWords.map(word =>{
-   const searchResult = wordsToSearch[word.replace(/[^a-zA-Z ]/g,'')];
-   wordDictionary[word] = (searchResult && searchResult !== '') ? searchResult : false;
+  justWords.map((word, index) => {
+    const searchResult = wordsToSearch[word.replace(/[^a-zA-Z ]/g, "")];
+    if (searchResult !== undefined && searchResult !== "") {
+      justWords[
+        index
+      ] = `<span class="define" data="${searchResult}">${word}</span>`;
+    }
   });
 
+  menuItem.DESCRIPTION_WITH_SPANS = justWords.join(" ");
 
-  let newDescription = '';
-  Object.keys(wordDictionary).map(word => {
-   if(wordDictionary[word]){
-     if(!definedWords.includes(word)){
-       newDescription = newDescription +
-         ` <span class="define" data="${wordDictionary[word]}">${word}</span>`;
-       definedWords.push(word);
-     }
-    }
-    else{
-      newDescription = newDescription + ' ' + word;
-    }
-
-  });
-
- menuItem.DESCRIPTION_WITH_SPANS = newDescription;
-
- return menuItem;
-
+  return menuItem;
 };
 
-const buildMenuPayload = (data) => {
-    let payload = data.items;
-    const newPayload = {}
-    payload.map(item => {
-      newPayload[item.ID] = transformMenuItem(item)
-    });
-    data.items = newPayload;
+const buildMenuPayload = data => {
+  let payload = data.items;
+  const newPayload = {};
+  payload.map(item => {
+    newPayload[item.ID] = transformMenuItem(item);
+  });
+  data.items = newPayload;
 
-    return data;
+  return data;
+};
 
-}
-
-const buildVenuePayload = (data) => {
+const buildVenuePayload = data => {
   return data.map(datum => {
     delete datum.dataValues.AIRTABLE_MENU_PAYLOAD;
     return datum.dataValues;
   });
-}
+};
 
 const menuTransformer = {
   getMenuData: data => buildMenuPayload(data),
   getVenueList: data => data,
-  getVenueNames: data => data,
+  getVenueNames: data => data
 };
 
 export default menuTransformer;
