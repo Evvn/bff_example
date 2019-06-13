d9519edf522427f759126af6e7f7e83eb3b9684d
Evan <evvn@outlook.com>
2019-06-13 10:35:21 +1000

    stringify webhook debugger

--- src/extractor/intentMapping/doshiiMapping/webhook.js
+++ src/extractor/intentMapping/doshiiMapping/webhook.js
@@ -23,8 +23,9 @@ const catchWebhook = (payload, onSuccess) => {
       if (event === "order_updated") {
         console.log("payload data:", payload.data);
         const { status, id, locationId } = payload.data;
-        console.log("/////" + payload);
-        console.log("#####" + order);
+
+        console.log("/////\n\n" + JSON.stringify(payload));
+        console.log("#####\n\n" + JSON.stringify(order));
 
         if (status === "accepted") {
           postToDatabase(`db/orders/updateStatus/${id}`, () => {}, {
